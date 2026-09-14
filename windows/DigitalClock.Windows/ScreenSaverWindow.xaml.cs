28930a19dec3169e538b91c0e6ece41c8f741b77
Soldatix <elvis.soldatic@gmail.com>
2026-09-14 18:10:34 +0200

    Add Digital Clock screen saver window

---
+++ windows/DigitalClock.Windows/ScreenSaverWindow.xaml.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.Web.WebView2.Core;
+
+namespace DigitalClock.Windows;
+
+public partial class ScreenSaverWindow : Window
+{
+    private readonly Stopwatch _startupStopwatch = Stopwatch.StartNew();
+    private readonly bool _isPreview;
+    private bool _closeRequested;
+
+    public ScreenSaverWindow(bool isPreview = false)
+    {
+        _isPreview = isPreview;
+        InitializeComponent();
+
+        if (_isPreview)
+        {
+            Width = 320;
+            Height = 240;
+            WindowState = WindowState.Normal;
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            ShowInTaskbar = true;
+        }
+        else
+        {
+            Topmost = true;
+        }
+
+        Loaded += ScreenSaverWindow_Loaded;
+        PreviewKeyDown += ScreenSaverWindow_PreviewKeyDown;
+        MouseMove += ScreenSaverWindow_MouseMove;
+    }
+
+    private async void ScreenSaverWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        await ScreenSaverWebView.EnsureCoreWebView2Async();
+        ScreenSaverWebView.CoreWebView2.WebMessageReceived += ScreenSaverWebView_WebMessageReceived;
+
+        string webFolder = Path.Combine(AppContext.BaseDirectory, "Web");
+        ScreenSaverWebView.CoreWebView2.SetVirtualHostNameToFolderMapping(
+            "digitalclock.local",
+            webFolder,
+            CoreWebView2HostResourceAccessKind.Allow
+        );
+
+        ScreenSaverWebView.Source = new Uri("https://digitalclock.local/index.html?screenSaver=1");
+    }
+
+    private void ScreenSaverWindow_PreviewKeyDown(object sender, KeyEventArgs e)
+    {
+        if (!_isPreview)
+        {
+            CloseScreenSaver();
+        }
+    }
+
+    private void ScreenSaverWindow_MouseMove(object sender, MouseEventArgs e)
+    {
+        if (!_isPreview && _startupStopwatch.ElapsedMilliseconds > 900)
+        {
+            CloseScreenSaver();
+        }
+    }
+
+    private void ScreenSaverWebView_WebMessageReceived(object? sender, CoreWebView2WebMessageReceivedEventArgs e)
+    {
+        try
+        {
+            using JsonDocument message = JsonDocument.Parse(e.WebMessageAsJson);
+            if (message.RootElement.TryGetProperty("action", out JsonElement action) &&
+                action.ValueKind == JsonValueKind.String &&
+                action.GetString() == "exitScreenSaver")
+            {
+                CloseScreenSaver();
+            }
+        }
+        catch (JsonException)
+        {
+            // Ignore malformed web messages from the hosted page.
+        }
+    }
+
+    private void CloseScreenSaver()
+    {
+        if (_closeRequested)
+        {
+            return;
+        }
+
+        _closeRequested = true;
+        Close();
+    }
+}
