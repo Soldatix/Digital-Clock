46cf7bcb48b3452ab1e65f98178bd442eeba763b
Soldatix <elvis.soldatic@gmail.com>
2026-09-14 18:10:32 +0200

    Handle Digital Clock screen saver arguments

--- windows/DigitalClock.Windows/App.xaml.cs
+++ windows/DigitalClock.Windows/App.xaml.cs
@@ -1,13 +1,24 @@
-﻿using System.Configuration;
-using System.Data;
+using System;
+using System.Linq;
 using System.Windows;
 
 namespace DigitalClock.Windows;
 
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
 public partial class App : System.Windows.Application
 {
-}
+    private void App_Startup(object sender, StartupEventArgs e)
+    {
+        string command = e.Args.FirstOrDefault()?.Trim().ToLowerInvariant() ?? string.Empty;
 
+        Window window = command switch
+        {
+            "/s" or "-s" => new ScreenSaverWindow(),
+            "/c" or "-c" => new ScreenSaverSettingsWindow(),
+            "/p" or "-p" => new ScreenSaverWindow(isPreview: true),
+            _ => new MainWindow()
+        };
+
+        MainWindow = window;
+        window.Show();
+    }
+}
